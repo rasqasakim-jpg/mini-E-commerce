d461a8ad7922a2a4c1eaf76cf99a855408e6f549
irgiharnoyo <rasqasakim@gmail.com>
2025-11-05 08:00:56 +0700

    E-comerce

--- App.tsx
+++ App.tsx
@@ -1,19 +1,8 @@
 import React from 'react';
-import { SafeAreaView, StatusBar, StyleSheet } from 'react-native';
-import GestureDemoScreen from './src/screens/GestureDemoScreen';
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar barStyle="light-content" backgroundColor="#007AFF" />
-      <GestureDemoScreen />
-    </SafeAreaView>
-  );
+    <>
+    </>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F4F6FA',
-  },
-});
